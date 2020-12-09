070d8f8514cc96166752a03ab19fc9427e12541e
ClementBuchanan <clement.buchanan@gmail.com>
2020-12-08 20:21:19 -0800

    updated for lab submission

--- js/app.js
+++ js/app.js
@@ -6,26 +6,37 @@ var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '
 
 
 //var containerParent = document.getElementById('container-parent');
-var seattleList = document.getElementById('seattle');
-var tokyoList = document.getElementById('tokyo');
-var dubaiList = document.getElementById('dubai');
-var parisList = document.getElementById('paris');
-var limaList = document.getElementById('lima');
+
+var table = document.getElementById('table');
+
+var seattle = new Location('Seattle', 23, 65, 6.3);
+var tokyo = new Location('Tokyo', 3, 24, 1.2);
+var dubai = new Location('Dubai', 11, 38, 3.7);
+var paris = new Location('Paris', 20, 38, 2.3);
+var lima = new Location('Lima', 2, 16, 4.6);
 
 
-var seattle = {
-  name: 'Seattle',
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  dailyTotals: 0,
-  hourlySalesArray: [],
+// Constructor function
 
-  getRandomCustomerCount: function () {
+seattle.intro();
+
+
+function Location(name, min, max, avg){
+  this.name = name,
+  this.min = min,
+  this.max = max,
+  this.avg = avg,
+  this.dailyTotals = 0,
+  this.hourlySalesArray = [],
+
+  this.intro = function(){
+    console.log(`Hello ${this.name}`);
+  },
+  this.getRandomCustomerCount = function () {
     return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
   },
 
-  calculateHourlySales() {
+  this.calculateHourlySales = function() {
     for (var i = 0; i < hours.length; i++) {
       var customerForTheHour = this.getRandomCustomerCount();
       var hourTotal = Math.ceil(customerForTheHour * this.avg);
@@ -33,171 +44,22 @@ var seattle = {
       this.dailyTotals += hourTotal;
     }
   },
-  render() {
+  this.render = function() {
     this.calculateHourlySales();
 
     for (var i = 0; i < hours.length; i++) {
       var liElement = document.createElement('li');
       liElement.textContent = hours[i] + ': ' + this.hourlySalesArray[i] + ' cookies';
-      seattleList.appendChild(liElement);
+      // seattleList.appendChild(liElement);
     }
     liElement = document.createElement('li');
     liElement.textContent = 'the total ' + this.dailyTotals;
-    seattleList.appendChild(liElement);
+    // seattleList.appendChild(liElement);
   }
-
 };
-
-var tokyo = {
-  name: 'Tokyo',
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  dailyTotals: 0,
-  hourlySalesArray: [],
-
-  getRandomCustomerCount: function () {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-
-  calculateHourlySales() {
-    for (var i = 0; i < hours.length; i++) {
-      var customerForTheHour = this.getRandomCustomerCount();
-      var hourTotal = Math.ceil(customerForTheHour * this.avg);
-      this.hourlySalesArray.push(hourTotal);
-      this.dailyTotals += hourTotal;
-    }
-  },
-  render() {
-    this.calculateHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var liElement = document.createElement('li');
-      liElement.textContent = hours[i] + ': ' + this.hourlySalesArray[i] + ' cookies';
-      seattleList.appendChild(liElement);
-    }
-    liElement = document.createElement('li');
-    liElement.textContent = 'the total ' + this.dailyTotals;
-    seattleList.appendChild(liElement);
-  }
-
-};
-
-var dubai = {
-  name: 'Dubai',
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  dailyTotals: 0,
-  hourlySalesArray: [],
-
-  getRandomCustomerCount: function () {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-
-  calculateHourlySales() {
-    for (var i = 0; i < hours.length; i++) {
-      var customerForTheHour = this.getRandomCustomerCount();
-      var hourTotal = Math.ceil(customerForTheHour * this.avg);
-      this.hourlySalesArray.push(hourTotal);
-      this.dailyTotals += hourTotal;
-    }
-  },
-  render() {
-    this.calculateHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var liElement = document.createElement('li');
-      liElement.textContent = hours[i] + ': ' + this.hourlySalesArray[i] + ' cookies';
-      seattleList.appendChild(liElement);
-    }
-    liElement = document.createElement('li');
-    liElement.textContent = 'the total ' + this.dailyTotals;
-    seattleList.appendChild(liElement);
-  }
-
-};
-
-var paris = {
-  name: 'Paris',
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  dailyTotals: 0,
-  hourlySalesArray: [],
-
-  getRandomCustomerCount: function () {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-
-  calculateHourlySales() {
-    for (var i = 0; i < hours.length; i++) {
-      var customerForTheHour = this.getRandomCustomerCount();
-      var hourTotal = Math.ceil(customerForTheHour * this.avg);
-      this.hourlySalesArray.push(hourTotal);
-      this.dailyTotals += hourTotal;
-    }
-  },
-  render() {
-    this.calculateHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var liElement = document.createElement('li');
-      liElement.textContent = hours[i] + ': ' + this.hourlySalesArray[i] + ' cookies';
-      seattleList.appendChild(liElement);
-    }
-    liElement = document.createElement('li');
-    liElement.textContent = 'the total ' + this.dailyTotals;
-    seattleList.appendChild(liElement);
-  }
-
-};
-
-var lima = {
-  name: 'Lima',
-  min: 23,
-  max: 65,
-  avg: 6.3,
-  dailyTotals: 0,
-  hourlySalesArray: [],
-
-  getRandomCustomerCount: function () {
-    return Math.floor(Math.random() * (this.max - this.min + 1) + this.min);
-  },
-
-  calculateHourlySales() {
-    for (var i = 0; i < hours.length; i++) {
-      var customerForTheHour = this.getRandomCustomerCount();
-      var hourTotal = Math.ceil(customerForTheHour * this.avg);
-      this.hourlySalesArray.push(hourTotal);
-      this.dailyTotals += hourTotal;
-    }
-  },
-  render() {
-    this.calculateHourlySales();
-
-    for (var i = 0; i < hours.length; i++) {
-      var liElement = document.createElement('li');
-      liElement.textContent = hours[i] + ': ' + this.hourlySalesArray[i] + ' cookies';
-      seattleList.appendChild(liElement);
-    }
-    liElement = document.createElement('li');
-    liElement.textContent = 'the total ' + this.dailyTotals;
-    seattleList.appendChild(liElement);
-  }
-
-};
-
-
 
 seattle.render();
 tokyo.render();
 dubai.render();
 paris.render();
 lima.render();
-
-seattle.renderList();
-tokyo.renderList();
-dubai.renderList();
-paris.renderList();
-lima.renderList();
